675ce724e6a84b90dd7532c6a858d150b6c7524e
ahtwtc <71689237+ahtwtc@users.noreply.github.com>
2020-11-24 15:48:07 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
-})
-radio.setGroup(10)
-basic.forever(function () {
-	
+let j = 0
+input.onGesture(Gesture.Shake, function () {
+    j = randint(0, 10)
+    basic.showNumber(j)
+    if (j >= 5) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
